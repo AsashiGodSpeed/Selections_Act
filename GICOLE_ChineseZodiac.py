408d4fc963706d09bd1b337b7f0ace558572d6b5
Aldrick James A. Gicole <144977698+AsashiGodSpeed@users.noreply.github.com>
2023-10-16 22:45:09 +0800

    GICOLE_ChineseZodiac.py

--- GICOLE_ChineseZodiac.py
+++ GICOLE_ChineseZodiac.py
@@ -1,18 +1,18 @@
-def chinese_zodiac(year):
+def chinese_zodiacsign(year):
     zodiac_animals = ["Rooster", "Dog", "Pig", "Rat", "Ox", "Tiger",
                       "Rabbit", "Dragon", "Snake", "Horse", "Sheep", "Monkey"]
     start_year = 1
 
     if year < start_year:
-        print("The Chinese zodiac cycle starts from 1924. Please enter a valid year.")
+        print("The Chinese zodiac cycle starts from 1. Please enter a valid year.")
     else:
         zodiac_index = (year - start_year) % 12
         zodiac_sign = zodiac_animals[zodiac_index]
-        print(f"The Chinese zodiac sign for the year {year} is {zodiac_sign}.")
+        print(f"The Chinese zodiac sign for your year {year} is {zodiac_sign}.")
 
 
 try:
-    year = int(input("Enter a year to find the Chinese zodiac sign: "))
-    chinese_zodiac(year)
+    year = int(input("Enter a year to find your Chinese zodiac sign: "))
+    chinese_zodiacsign(year)
 except ValueError:
     print(" Please enter a year: ")
